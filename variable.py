d7b80ee470fe16df8ed63b60303bcb86b693fac6
Himanshu <himanshupa321@gmail.com>
2023-04-14 17:00:58 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,13 @@
+#multiple value to multiple variable
+a,b,c=30,40,50
+print(a,b,c)
+a=8
+b=9
+c=12
+print(a,b,c)
+#single value to multiple variable
+a=b=c=3
+print(a,b,c)
+#variable should be capital or small or start with "_" ;
+himanshu="pathak"
+print(himanshu)
